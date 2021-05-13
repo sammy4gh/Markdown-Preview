46373573bfbc1d86fbf457bd58196bd38f697927
sammy <67767655+sammy4gh@users.noreply.github.com>
2021-05-13 13:28:42 +0000

    Removed a line

--- src/Preview.js
+++ src/Preview.js
@@ -2,19 +2,11 @@ import React from 'react';
 import './App.css';
 import { FaEye } from "react-icons/fa";
 import marked from 'marked';
-import parse from 'html-react-parser';
+
 
 const Preview = ({input}) => {
 
-    const renderer = {
-        link(href, title, text) {
-            return `<a target="_blank" href="${href}">${text}` + '</a>';
-        }
-    };
-
-//applies the adjusted link
-    marked.use({ renderer });
-
+   
 //allows carriage returns to be rendered as breaks
     marked.setOptions({
         breaks: true,
